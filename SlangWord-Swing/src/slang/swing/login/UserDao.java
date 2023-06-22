fc4691d7e126080fd0c3e28a1aaff7d115bc722e
huytd-it <yukihuy9999@gmail.com>
2023-06-22 23:38:07 +0700

    finish login

---
+++ SlangWord-Swing/src/slang/swing/login/UserDao.java
@@ -0,0 +1,13 @@
+package slang.swing.login;
+
+public class UserDao {
+    public boolean checkUser(User user) {
+        if (user != null) {
+            if ("admin".equals(user.getUserName())
+                    && "admin".equals(user.getPassword())) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
